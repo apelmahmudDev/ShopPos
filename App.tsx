f323e8f137af957e13517be56a8efadec9db049a
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-09-27 14:39:01 +0600

    [feat] Add husky/pre-commit & lint-staged

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 // App.tsx
 import React from 'react';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
-import {NavigationContainer} from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
 // import {ThemeProvider, useAppTheme} from '@/theme';
 // import {NavDark, NavLight} from '@/theme/navThemes';
 // import RootNavigator from '@/navigation/RootNavigator';
 import { ThemeProvider, useAppTheme } from '@theme/index';
 import { NavDark, NavLight } from '@theme/navThemes';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 function AppNav() {
-  const {isDark} = useAppTheme();
+  const { isDark } = useAppTheme();
   return (
     <NavigationContainer theme={isDark ? NavDark : NavLight}>
       {/* <RootNavigator /> */}
-      <Text>Hi there Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque itaque unde facilis modi soluta voluptatum aspernatur placeat nisi dicta? Voluptatum porro necessitatibus commodi sed deserunt suscipit accusantium, numquam beatae distinctio.</Text>
+      <View
+        style={{
+          height: 500,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Text style={{ color: '#fff', fontSize: 24 }}>Shop Pos</Text>
+      </View>
     </NavigationContainer>
   );
 }
