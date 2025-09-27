5147b27c1747134c49baf8a6cf4cc027af9a4c1a
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-09-27 15:38:07 +0600

    [feat] Setup Eslint + Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,22 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@react-native',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['jest'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      env: { 'jest/globals': true },
+    },
+  ],
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,10 @@ module.exports = {
   arrowParens: 'avoid',
   singleQuote: true,
   trailingComma: 'all',
+  semi: true,
+  printWidth: 80,
+  tabWidth: 2,
+  bracketSpacing: true,
+  jsxSingleQuote: false,
+  endOfLine: 'lf',
 };
