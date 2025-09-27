b72f1c4ee71ebc80cbe890663f3bff6c8bf88dab
Apel Mahmud <apelmahmud.dev@gmail.com>
2025-09-27 16:54:01 +0600

    [feat] Install dependency and babel config

--- babel.config.js
+++ babel.config.js
@@ -1,19 +1,27 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
+    ['react-native-reanimated/plugin'],
+    ['module:react-native-dotenv'],
     [
       'module-resolver',
       {
         root: ['./src'],
         extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
         alias: {
-          "@components": "./src/components",
-          "@screens": "./src/screens",
-          "@utils": "./src/utils",
-          "@theme": "./src/theme",
-          "@navigation": "./src/navigation",
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils',
+          '@theme': './src/theme',
+          '@navigation': './src/navigation',
         },
       },
     ],
   ],
+};
+
+// babel.config.js
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: ['react-native-reanimated/plugin'],
 };
